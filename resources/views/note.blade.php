7b1781d4a4fea243648492f97a25f5fc55d8e1f6
Evgenii Mironov <mir-evgenii@mail.ru>
2021-06-16 11:58:23 +0300

    Added edit and delete button in main notes page.

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -9,22 +9,34 @@
     @csrf
     <input type='title' name='title' value='{{ $note->title }}' id='title' class='form-control'><br>
     <textarea name='content' id='content' class='form-control'>{{ $note->content }}</textarea><br>
-    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-        <div class="btn-group me-2" role="group">
-            <a class="btn btn-secondary" href="/note" role="button">Cancel</a>
-        </div>
-        <div class="btn-group me-2" role="group">
-            <button type='submit' class='btn btn-success'>Save</button>
-        </div>
+    <div class="btn-group" role="group">
+        <a class="btn btn-outline-secondary" href="/note" role="button">Cancel</a>
+        <button type='submit' class='btn btn-outline-success'>Save</button>
+        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#delNoteModal">Delete</button>
+    </div>
 </form>
 
-        <div class="btn-group" role="group">
-            <form method='post' action='/note/{{ $note->id }}'> 
-                @csrf
-                @method('DELETE')
-                <button type='submit' class='btn btn-danger'>Delete</button>
-            </form>
+<!-- Modal delete note -->
+<div class="modal fade" id="delNoteModal" tabindex="-1" aria-labelledby="delNoteModal" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Delete note</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-
+        <div class="modal-body">
+          <h6>Do you really want to delete note: "{{ $note->title }}"?</h6>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <form method='post' action='/note/{{ $note->id }}'> 
+            @csrf
+            @method('DELETE')
+            <button type='submit' class='btn btn-danger'>Delete</button>
+          </form>
+        </div>
+      </div>
     </div>
+</div>
+
 @endsection
--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -4,19 +4,49 @@
 
 @section('main_layout_content')
 
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-    @foreach($notes as $note)
-      <div class="col">
-        <div class="card text-dark bg-light">
-          <!-- <div class="card-header"></div> -->
-          <div class="card-body">
-            <h5 class="card-title">{{ $note->title }}</h5>
-            <p class="card-text">{{ $note->content }}</p>
-          </div>
-          <div class="card-footer"><small class="text-muted"><em>{{ $note->updated_at }}</em></small></div>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+@foreach($notes as $note)
+  <div class="col">
+    <div class="card text-dark bg-light">
+      <div class="card-header">
+        <div class="btn-group me-2">
+          <a class="btn btn-outline-secondary" href="/note/{{ $note->id }}" role="button"><i class="bi bi-pencil"></i></a>
+          <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#delNoteModal{{ $note->id }}"><i class="bi bi-trash"></i></button>
         </div>
       </div>
-    @endforeach
+      <div class="card-body">
+        <h5 class="card-title">{{ $note->title }}</h5>
+        <p class="card-text">{{ $note->content }}</p>
+      </div>
+      <div class="card-footer"><small class="text-muted"><em>{{ $note->updated_at }}</em></small></div>
     </div>
+  </div
+
+  <!-- Modal delete note -->
+  <div class="modal fade" id="delNoteModal{{ $note->id }}" tabindex="-1" aria-labelledby="delNoteModal{{ $note->id }}" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Delete note</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <h6>Do you really want to delete note: "{{ $note->title }}"?</h6>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <form method='post' action='/note/{{ $note->id }}'> 
+            @csrf
+            @method('DELETE')
+            <button type='submit' class='btn btn-danger'>Delete</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+@endforeach
+</div>
+
+
 
 @endsection
