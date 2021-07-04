e9eac1e93efd7de620471ba5ac7234574711ff01
Evgenii Mironov <mir-evgenii@mail.ru>
2021-07-04 19:28:46 +0300

    Added run bot function.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () { TelegramBot::send('Hi!', '123'); })->everyMinute();
+        $schedule->call(function () { TelegramBot::runBot(); })->everyMinute();
     }
 
     /**
--- app/Http/TelegramBot/TelegramBot.php
+++ app/Http/TelegramBot/TelegramBot.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\TelegramBot;
 
+use App\Models\NoteNotifications;
+use App\Models\Telegram;
+use App\Models\Note;
+use App\Models\User;
+use DateInterval;
+use DateTime;
+
 class TelegramBot
 {
     private const TOKEN = 'TELEGRAM_BOT_TOKEN'; // this is variable name in .env file
@@ -11,15 +18,35 @@ class TelegramBot
         return self::APILINK . env(self::TOKEN) . '/';
     }
 
-    public static function getUpdate() {
+    private static function getUpdate() {
         $update = file_get_contents(self::getApiLink() . 'getUpdates');
         $updateArray = json_decode($update, TRUE);
 
         return $updateArray;
     }
 
-    public static function send($message, $chatId) {
+    private static function send($message, $chatId) {
         file_get_contents(self::getApiLink() . "sendmessage?chat_id={$chatId}&text={$message}");
     }
 
+    public static function runBot() {
+        $date = new DateTime();
+        $now = $date->format('Y-m-d H:i:s');
+        $date->add(new DateInterval('PT1M'));
+        $future = $date->format('Y-m-d H:i:s');
+
+        $notifications = NoteNotifications::whereBetween('notify_at', [$now, $future])->get();
+
+        if ($notifications != null) {
+            foreach ($notifications as $value) {
+                $note = Note::find($value->note_id);
+                $telegram = Telegram::where('user_id', $note->user_id)->first();
+                if ($note != null && $telegram != null) {
+                    $message = $note->title.' '.$note->content;
+                    $chatId = $telegram->chat_id;
+                    self::send($message, $chatId);
+                }
+            }
+        }
+    }
 }
