adbf82546a0827c3233b32520ac930e07c70077c
Evgenii Mironov <mir-evgenii@mail.ru>
2021-07-10 14:13:14 +0300

    Install dusk tests, and make login test.

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email', env('TEST_USER_1_EMAIL'))
+                ->type('password', env('TEST_USER_1_PASSWORD'))
+                ->press('Log in')
+                ->assertPathIs('/dashboard');;
+        });
+    }
+}
